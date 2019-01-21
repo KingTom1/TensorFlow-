80c8743edac022f0178ec06cc3926542a9843b2d
KingTom1 <38772091+KingTom1@users.noreply.github.com>
2019-01-21 16:18:15 +0800

    20190116

--- TensorFlow进阶/20190121/MNIST数字识别问题.py
+++ TensorFlow进阶/20190121/MNIST数字识别问题.py
@@ -1,12 +1,12 @@
-'''Tensorflow专门提供了一个类 读取MNIST数据集和处理成需要的数据格式'''
+'''Tensorflow专门提供了一个类 读取MNIST数据集和处理成需要的数据格式  read_data_sets无法下载数据源'''
 
 from tensorflow.examples.tutorials.mnist import input_data
 
 # 载入MNIST数据集,如果本地指定路径没有下载好的数据,将自动下载MNIST数据集
 mnist = input_data.read_data_sets("/path/to/MNIST_data/",one_hot=True)
 
-# 打印训练数据 Training data size: 55000
+# 打印训练数据: 55000
 print("Training data size: ",mnist.train.num_examples)
 
-# 打印验收数据 Validating data size: 5000
+# 打印验收数据: 5000
 print("Validating data size: ",mnist.validation.num_examples)
